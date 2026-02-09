f516d8b6f57510b3d2eac6b65f291a9ca0dab3ee
AfricaX <162136807+AfricaX@users.noreply.github.com>
2026-02-09 13:23:00 +0800

    Implement message alerts in update function
    Added message timer to show alerts based on position.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let pos = 1; 
+let messageTimer;
 const char = document.getElementById('character');
 const couple = document.querySelector('img[alt="Yes"]');
 const sad = document.querySelector('img[alt="No"]')
@@ -14,16 +15,29 @@ function moveRight() {
 }
 
 function update() {
+
+clearTimeout(messageTimer);
+    
     couple.classList.remove('revealed', 'zoom-shake');
     sad.classList.remove('revealed', 'zoom-shake');
 
     if (pos === 0) {
         char.style.left = "var(--yes-spot)"; 
         couple.classList.add('revealed', 'zoom-shake');
+
+messageTimer = setTimeout(() => {
+            alert("Yay! I knew it! Sabi ko sayo crush mo ko e, Happy Valentines Day! I love you ❤️");
+        }, 3000);
+        
     } else if (pos === 1) {
         char.style.left = "var(--mid-spot)";
     } else {
         char.style.left = "var(--no-spot)";
         sad.classList.add('revealed', 'zoom-shake');
+
+messageTimer = setTimeout(() => {
+            alert("Still thinking about 'No'? Why? 😢");
+        }, 3000);  
     }
+
 }
